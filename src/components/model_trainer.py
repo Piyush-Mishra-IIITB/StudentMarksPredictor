239c27cc15c8764f7ec4c3bb970233f7bd0990d0
Piyush mishra <piyushmishra8982@gmail.com>
2025-12-03 19:18:20 +0530

    deployment config

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,7 +41,7 @@ class ModelTrainer:
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
-                "Linear Regression": LinearRegression(),
+                
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
@@ -64,11 +64,6 @@ class ModelTrainer:
                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
                     # 'criterion':['squared_error', 'friedman_mse'],
                     # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Linear Regression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "CatBoosting Regressor":{
